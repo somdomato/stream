db892031fad9baefdddc23ef93265f6c17662100
Lucas Saliés Brum <sistematico@gmail.com>
2024-07-07 19:42:58 -0400

    Update

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,8 @@
 
 if [ -f /etc/arch-release ]; then
   ansible-playbook -e "ansible_port=2200 ansible_python_interpreter=/usr/bin/python3" /home/lucas/code/somdomato/stream/ansible/playbook.yml -i ananke.paxa.dev,
+elif [ -f /etc/debian_version ]; then
+  ansible-playbook -e "ansible_port=2200 ansible_python_interpreter=/usr/bin/python3" /home/lucas/code/somdomato/stream/ansible/playbook.yml -i ananke.paxa.dev,
 else
   ansible-playbook --connection=local -e "ansible_port=2200" /var/www/somdomato/ansible/playbook.yml -i localhost,
 fi

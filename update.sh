0f630b35ddd47010c1938bbac4dbe2938b80438b
Lucas Saliés Brum <sistematico@gmail.com>
2024-07-10 05:31:09 -0400

    Update

--- update.sh
+++ update.sh
@@ -2,3 +2,8 @@
 
 scp ansible/etc/icecast2/*.xml root@ananke:/etc/icecast2/
 scp ansible/etc/liquidsoap/*.liq root@ananke:/etc/liquidsoap/
+scp ansible/etc/systemd/system/liquidsoap.service root@ananke:/etc/systemd/system/
+scp ansible/usr/local/bin/next-song root@ananke:/usr/local/bin/
+
+ssh root@ananke "systemctl daemon-reload"
+ssh root@ananke "systemctl restart liquidsoap"

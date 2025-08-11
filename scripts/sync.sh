d41c94cc53740e60616df2c6dee488f475b800bc
Lucas Saliés Brum <sistematico@gmail.com>
2025-08-11 08:36:16 -0400

    Update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-MACHINE="eris"
-SONGS_PATH="/media/songs"
-
-rsync -avzz /home/lucas/audio/sdm/ root@$MACHINE:$SONGS_PATH/ --delete
-
-ssh root@$MACHINE "chown -R liquidsoap:liquidsoap $SONGS_PATH && chown -R nginx:nginx $SONGS_PATH/uploads"
-ssh root@$MACHINE "find $SONGS_PATH -type d -exec chmod 755 '{}' \; && find $SONGS_PATH -type f -exec chmod 644 '{}' \;"
+MACHINE="tyche"
+REMOTE_SONGS_PATH="/var/music"
+[ "$(uname -s)" == "Darwin" ] && \
+    LOCAL_SONGS_PATH="/Users/lucas/Music/sdm" || \
+    LOCAL_SONGS_PATH="/home/lucas/music/sdm"
+DELETE=""
+ssh root@$MACHINE "mkdir -p $REMOTE_SONGS_PATH"
+rsync -avzz $LOCAL_SONGS_PATH/ root@$MACHINE:$REMOTE_SONGS_PATH/ $DELETE
+ssh root@$MACHINE "chown -R liquidsoap:liquidsoap $REMOTE_SONGS_PATH"
+ssh root@$MACHINE "find $REMOTE_SONGS_PATH -type d -exec chmod 755 '{}' \;"
+ssh root@$MACHINE "find $REMOTE_SONGS_PATH -type f -exec chmod 644 '{}' \;"
 ssh root@$MACHINE "systemctl restart icecast2-somdomato liquidsoap-somdomato"
